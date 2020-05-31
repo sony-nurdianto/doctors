6eab78eee39af7a00380eb31b085bb6f63145380
sony-nurdianto <sonynurdianto445@gmail.com>
2020-05-31 19:56:37 +0700

    oke

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Style.css";
+
+const Home = () => {
+  return (
+    <div className="backGround">
+      <div className="jumbotron">
+        <div className="container">
+          <div className="row">
+            <div className="col">halo</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
